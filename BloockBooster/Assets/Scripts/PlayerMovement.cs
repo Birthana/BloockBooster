ed3f3dc9549b09cc65e45d2988ed2ed0b1f21162
Thana Sithisombath <sithisombath@cpp.edu>
2020-02-01 14:48:45 -0800

    added stage1 scripts. platforms, barrels, etc.

--- BloockBooster/Assets/Scripts/PlayerMovement.cs
+++ BloockBooster/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] float speed;
-    [SerializeField] float jumpHeight;
+    [SerializeField] float jumpForce;
     public Animator animator;
 
-
-    private int StageNum;
+    private bool isJumping;
+    private int StageNum = 1;
+    private float fallMultiplier = 2.5f;
+    private float lowJumpMultiplier = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +34,25 @@ public class PlayerMovement : MonoBehaviour
 
     void Stage1Control()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0) * speed;
-        if(Input.GetButtonDown("SPACE"))
-            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), jumpHeight) * speed;
+        float h = Input.GetAxis("Horizontal");
+        animator.SetFloat("horizontal", h);
+        Vector2 tempVect = new Vector2(h, 0);
+        animator.SetFloat("speed", tempVect.sqrMagnitude);
+        rb.velocity = new Vector2(h * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
     }
     void Stage2Control()
     {
@@ -56,4 +74,15 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 contactPoint = collision.contacts[0].point;
+        Vector3 center = collision.collider.bounds.center;
+
+        if(contactPoint.y > center.y)
+        {
+            isJumping = false;
+            animator.SetBool("isJumping", false);
+        }
+    }
 }
---
+++ BloockBooster/Assets/Scripts/Stage1/BarrelSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelSpawner : MonoBehaviour
+{
+    public GameObject barrelPrefab;
+    public float cooldown;
+    public bool isOnCooldown;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!isOnCooldown)
+        {
+            StartCoroutine(RollingBarrel());
+        }
+    }
+
+    IEnumerator RollingBarrel()
+    {
+        isOnCooldown = true;
+        animator.SetBool("IsOpen", true);
+        yield return new WaitForSeconds(1.0f);
+        animator.SetBool("IsOpen", false);
+        GameObject barrel = Instantiate(barrelPrefab, this.transform.position + new Vector3(1.5f, 0f, 0f), Quaternion.identity);
+        yield return new WaitForSeconds(cooldown);
+        cooldown = Random.Range(1, 4);
+        isOnCooldown = false;
+    }
+}
