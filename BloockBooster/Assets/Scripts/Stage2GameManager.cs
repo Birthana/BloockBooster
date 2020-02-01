e5c6587b05720ace4435dc69fd8350680c68a52e
Thana Sithisombath <sithisombath@cpp.edu>
2020-02-01 00:29:03 -0800

    added manage for player healtha nd timer for stage 2.

---
+++ BloockBooster/Assets/Scripts/Stage2GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage2GameManager : MonoBehaviour
+{
+    public int playerHealth;
+    public float maxTimer;
+
+    private float health;
+    private float timer;
+
+    private void Start()
+    {
+        health = playerHealth;
+        timer = maxTimer;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Debug.Log("Timer End.");
+        }
+    }
+
+    public void TakeDamage()
+    {
+        health--;
+        if (health == 0)
+        {
+            Debug.Log("Player dies.");
+        }
+    }
+}
---
+++ BloockBooster/Assets/Scripts/Stage2Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage2Player : MonoBehaviour
+{
+    public Stage2GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<Stage2GameManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Stage2Bullet>())
+        {
+            Destroy(collision.gameObject);
+            gameManager.TakeDamage();
+        }
+    }
+}
