cfabd3d70ae63887bf920cd87677d0a4c4eac135
Thana Sithisombath <sithisombath@cpp.edu>
2020-01-31 20:35:18 -0800

    added Player movement.

---
+++ BloockBooster/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 tempVect = new Vector2(h, v);
+
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + tempVect * speed);
+    }
+}
