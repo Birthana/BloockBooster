80b599aa353cd965a6cbacc437ad9bad9609fa3e
Jonathan Chhou <chhouj@uci.edu>
2020-02-02 13:52:04 -0800

    Stage 4 updated

---
+++ BloockBooster/Assets/Scripts/Stage4EnemyManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage4EnemyManager : MonoBehaviour
+{
+    float spawnDelay;
+    float time = 0f;
+    
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnDelay = Random.Range(5.0f, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= spawnDelay)
+        {
+            time = 0;
+            spawnDelay = Random.Range(5.0f, 10.0f);
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject b = this.GetComponent<ObjectPool>().FindUnusedObject();
+        b.SetActive(true);
+        b.transform.localPosition = Vector3.Normalize((player.transform.position - this.GetComponent<ObjectPool>().transform.position));
+    }
+}
