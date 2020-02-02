1c10deade90d56a5945b95af4aa65e2a8abfdc1f
Jonathan Chhou <chhouj@uci.edu>
2020-02-02 14:46:44 -0800

    stage 3 building.

---
+++ BloockBooster/Assets/Scripts/Stage3Event.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage3Event : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIManager.instance.SetMaxHealth(5);
+        UIManager.instance.SetTimer(0,4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
